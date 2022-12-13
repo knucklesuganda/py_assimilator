174db3866864b64bedf9d95f01cedd0ed9a4bdb7
knucklesuganda <testaddressss43@gmail.com>
2022-12-13 19:01:33 +0100

    Fixed unit of work abstract methods

--- assimilator/internal/__init__.py
+++ assimilator/internal/__init__.py
@@ -1,1 +1,4 @@
-import assimilator.internal.events.events_bus
+from assimilator.internal.events.events_bus import *
+from assimilator.internal.database.repository import *
+from assimilator.internal.database.unit_of_work import *
+from assimilator.internal.database.specifications import *
--- assimilator/internal/database/unit_of_work.py
+++ assimilator/internal/database/unit_of_work.py
@@ -1,26 +1,21 @@
-from abc import ABC, abstractmethod
-
+from assimilator.core.database import UnitOfWork
 from assimilator.core.database.repository import BaseRepository
 
 
-class DictUnitOfWork(ABC):
+class DictUnitOfWork(UnitOfWork):
     def __init__(self, repository: BaseRepository):
-        self.repository = repository
+        super(DictUnitOfWork, self).__init__(repository)
         self._saved_data = None
 
-    @abstractmethod
     def begin(self):
         self._saved_data = self.repository.session
         self.repository.session = dict(self._saved_data)
 
-    @abstractmethod
     def rollback(self):
         self.repository.session = self._saved_data
 
-    @abstractmethod
     def commit(self):
         self._saved_data = self.repository.session
 
-    @abstractmethod
     def close(self):
         pass
